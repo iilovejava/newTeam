08bf98057382a6af33afa41d9e4731391f92d551
iilovejava <love774065425@163.com>
2018-07-26 15:45:37 +0800

    project

---
+++ src/main/java/com/lanou/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.lanou.controller;
+
+import com.lanou.model.User;
+import com.lanou.service.Userservice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+  //我改了
+
+}
